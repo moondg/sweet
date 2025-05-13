5a622caa642c691c9f868871eed14c5c2183f2e9
moondg <moondg07@outlook.com>
2025-05-13 22:00:24 +0900

    fix: linear region searching algorithm

--- modules/findLinearRegion.py
+++ modules/findLinearRegion.py
@@ -3,20 +3,20 @@ import modules.parameters as p
 
 def findLinearRegion(yData):
     # smooth the data
-    ySmooth = np.convolve(yData, np.ones(p.window_size)/p.window_size, mode='valid')
+    ySmooth = np.convolve(yData, np.ones(p.window_size)/p.window_size, mode='same')
 
     gradient = np.gradient(ySmooth)
     curvature = np.gradient(gradient)
 
     flatStart, flatEnd, linStart, linEnd = len(gradient), len(gradient), None, len(gradient)
 
-    # iterate from back
-    for i,(c,g) in enumerate(zip(np.flip(curvature), np.flip(gradient))):
-        if g < p.gradient_threshold and i < 100:
-            flatStart = flatStart - 1
-            linEnd = flatStart
+    isFlatEnd = False
+    for i,(c,g) in reversed(list(enumerate(zip(curvature,gradient)))):
+        if not isFlatEnd and p.gradient_threshold < g:
+            isFlatEnd = True
+            flatStart = linEnd = i
         if p.curvature_threshold < c:
-            linStart = len(gradient) - i - 1
+            linStart = i
             break
 
     return  linStart, linEnd, flatStart, flatEnd
